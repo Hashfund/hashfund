f6b0a78edc20f324be9f362513a11365ccbb81f4
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-10-10 06:49:46 +0100

    chore: packages/bn not a submodule

--- packages/zeroboost/src/index.ts
+++ packages/zeroboost/src/index.ts
@@ -1,10 +1,10 @@
-import Zeroboost from "../target/idl/zeroboost.json";
+import Zeroboost from "./idl/zeroboost.json";
 
 export * from "./pda";
 export * from "./utils";
 export * from "./curve";
 export * from "./config";
 export * from "./instructions";
-export type { Zeroboost } from "./target/types/zeroboost";
+export type { Zeroboost } from "./types/zeroboost";
 
-export const IDL = Zeroboost as unknown as typeof import("./target/types/zeroboost").IDL;
+export const IDL = Zeroboost as unknown as typeof import("./types/zeroboost").IDL;
