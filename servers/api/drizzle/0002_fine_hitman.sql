0dbfbb49ce870fd9b6fbcccc4cc3bb5f930f9507
ApexKingOnly <shilleazy@gmail.com>
2026-04-18 14:17:03 -0700

    fix(db): make migration 0002 idempotent with DO EXCEPTION guards

--- servers/api/drizzle/0002_fine_hitman.sql
+++ servers/api/drizzle/0002_fine_hitman.sql
@@ -1,17 +1,67 @@
-ALTER TABLE "mints" ALTER COLUMN "supply" SET DATA TYPE numeric(40, 0);--> statement-breakpoint
-ALTER TABLE "swaps" ALTER COLUMN "token_amount" SET DATA TYPE numeric(40, 0);--> statement-breakpoint
-ALTER TABLE "swaps" ALTER COLUMN "pair_amount" SET DATA TYPE numeric(40, 0);--> statement-breakpoint
-ALTER TABLE "swaps" ALTER COLUMN "market_cap" SET DATA TYPE numeric(40, 0);--> statement-breakpoint
-ALTER TABLE "swaps" ALTER COLUMN "virtual_token_balance" SET DATA TYPE numeric(40, 0);--> statement-breakpoint
-ALTER TABLE "swaps" ALTER COLUMN "virtual_pair_balance" SET DATA TYPE numeric(40, 0);--> statement-breakpoint
-ALTER TABLE "boundingCurve" ALTER COLUMN "minimum_pair_balance" SET DATA TYPE numeric(40, 0);--> statement-breakpoint
-ALTER TABLE "boundingCurve" ALTER COLUMN "maximum_pair_balance" SET DATA TYPE numeric(40, 0);--> statement-breakpoint
-ALTER TABLE "boundingCurve" ALTER COLUMN "virtual_token_balance" SET DATA TYPE numeric(40, 0);--> statement-breakpoint
-ALTER TABLE "boundingCurve" ALTER COLUMN "virtual_pair_balance" SET DATA TYPE numeric(40, 0);--> statement-breakpoint
-ALTER TABLE "boundingCurve" ADD COLUMN "initial_supply" numeric(40, 0) NOT NULL;--> statement-breakpoint
-ALTER TABLE "boundingCurve" ADD COLUMN "net_active_capital" numeric(40, 0) NOT NULL;--> statement-breakpoint
-ALTER TABLE "boundingCurve" ADD COLUMN "total_contributed" numeric(40, 0) NOT NULL;--> statement-breakpoint
-ALTER TABLE "boundingCurve" ADD COLUMN "total_burned_tokens" numeric(40, 0) NOT NULL;--> statement-breakpoint
-ALTER TABLE "boundingCurve" ADD COLUMN "total_fees_collected" numeric(40, 0) NOT NULL;--> statement-breakpoint
-ALTER TABLE "boundingCurve" ADD COLUMN "bump" integer NOT NULL;--> statement-breakpoint
-ALTER TABLE "boundingCurve" ADD COLUMN "reserve_bump" integer NOT NULL;
+DO $$ BEGIN
+  ALTER TABLE "mints" ALTER COLUMN "supply" SET DATA TYPE numeric(40, 0);
+EXCEPTION WHEN others THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "swaps" ALTER COLUMN "token_amount" SET DATA TYPE numeric(40, 0);
+EXCEPTION WHEN others THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "swaps" ALTER COLUMN "pair_amount" SET DATA TYPE numeric(40, 0);
+EXCEPTION WHEN others THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "swaps" ALTER COLUMN "market_cap" SET DATA TYPE numeric(40, 0);
+EXCEPTION WHEN others THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "swaps" ALTER COLUMN "virtual_token_balance" SET DATA TYPE numeric(40, 0);
+EXCEPTION WHEN others THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "swaps" ALTER COLUMN "virtual_pair_balance" SET DATA TYPE numeric(40, 0);
+EXCEPTION WHEN others THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "boundingCurve" ALTER COLUMN "minimum_pair_balance" SET DATA TYPE numeric(40, 0);
+EXCEPTION WHEN others THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "boundingCurve" ALTER COLUMN "maximum_pair_balance" SET DATA TYPE numeric(40, 0);
+EXCEPTION WHEN others THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "boundingCurve" ALTER COLUMN "virtual_token_balance" SET DATA TYPE numeric(40, 0);
+EXCEPTION WHEN others THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "boundingCurve" ALTER COLUMN "virtual_pair_balance" SET DATA TYPE numeric(40, 0);
+EXCEPTION WHEN others THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "boundingCurve" ADD COLUMN "initial_supply" numeric(40, 0) NOT NULL;
+EXCEPTION WHEN duplicate_column THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "boundingCurve" ADD COLUMN "net_active_capital" numeric(40, 0) NOT NULL;
+EXCEPTION WHEN duplicate_column THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "boundingCurve" ADD COLUMN "total_contributed" numeric(40, 0) NOT NULL;
+EXCEPTION WHEN duplicate_column THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "boundingCurve" ADD COLUMN "total_burned_tokens" numeric(40, 0) NOT NULL;
+EXCEPTION WHEN duplicate_column THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "boundingCurve" ADD COLUMN "total_fees_collected" numeric(40, 0) NOT NULL;
+EXCEPTION WHEN duplicate_column THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "boundingCurve" ADD COLUMN "bump" integer NOT NULL;
+EXCEPTION WHEN duplicate_column THEN null; END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+  ALTER TABLE "boundingCurve" ADD COLUMN "reserve_bump" integer NOT NULL;
+EXCEPTION WHEN duplicate_column THEN null; END $$;
